baee61b3df8490ab2b8e079a783bdfb20c65a0ac
Harry Collin <harry.archie.collin@gmail.com>
2017-09-10 13:53:32 +0100

    Unity Project Changes

--- NCode.Unity/Assets/Scenes/AutoConnect/AutoConnect.cs
+++ NCode.Unity/Assets/Scenes/AutoConnect/AutoConnect.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
+﻿using NCode.Core.Client;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoConnect : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        NetworkManager.Connect("127.0.0.1", 5127);	
 	}
 	
 	// Update is called once per frame
