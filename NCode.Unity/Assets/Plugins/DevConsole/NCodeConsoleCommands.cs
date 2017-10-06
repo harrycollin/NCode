c9367f8c6314b8585b4bee029f55a808a62d9d56
Harry Collin <harry.archie.collin@gmail.com>
2017-10-06 11:12:34 +0100

    Transfer entity support added to client console

--- NCode.Unity/Assets/Plugins/DevConsole/NCodeConsoleCommands.cs
+++ NCode.Unity/Assets/Plugins/DevConsole/NCodeConsoleCommands.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 using SickDev.CommandSystem;
 using DevConsole;
 using NCode.Client;
+using NCode.Core.Entity;
+using System.Collections.Generic;
 
 public class NCodeConsoleCommands : MonoBehaviour {
+
+    static List<GameObject> networkObjects = new List<GameObject>();
+
+    static NNetworkEntity selectedEntity;
 
     [Command]
     static void Connect(string ip, int port)
@@ -33,5 +39,32 @@ public class NCodeConsoleCommands : MonoBehaviour {
     static void SpawnEntity(int id)
     {
         NetworkManager.CreateEntity(1, NetworkManager.GetPrefab(id), Vector3.zero, Quaternion.identity);
+    }
+
+    [Command]
+    static void ListEntities()
+    {
+        networkObjects.Clear();
+        foreach (var go in FindObjectsOfType<GameObject>())
+        {
+            if (go.GetComponent<NEntityLink>()) networkObjects.Add(go);
+        }
+
+        for(int i = 0; i < networkObjects.Count; i++)
+        {
+            Console.print(string.Format("Index: {0}, Name: {1}", i, networkObjects[i].name));
+        }
+    }
+
+    [Command]
+    static void SelectEntity(int index)
+    {
+        selectedEntity = NetworkManager.GetEntity(networkObjects[index].GetComponent<NEntityLink>().Guid);
+    }
+
+    [Command]
+    static void TransferEntity(int channela, int channelb)
+    {
+        NetworkManager.TransferEntity(selectedEntity.Guid, channela, channelb);
     }
 }
